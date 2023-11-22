f7cfd9b5eef9ae6b4ad6f8dee4cce3b3e4f44004
Totktosmog <totktosmog513@gmail.com>
2023-11-22 19:13:04 +0300

    Добавьте файлы проекта.

---
+++ C_Sharp_LINQ_lab_1/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace C_Sharp_LINQ_lab_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task1();
+        }
+
+        /// <summary>
+        /// Дана целочисленная последовательность, содержащая как положительные,
+        /// так и отрицательные числа.Вывести ее первый положительный элемент и последний
+        /// отрицательный элемент.
+        /// </summary>
+        private static void Task1()
+        {
+            int[] array = { -1, 2, -13, 4, 5, 6, -17, 8, };
+
+            int firstPositive = array.Where(n => n > 0).First();
+            int lastNegative = array.Where(n => n < 0).Last();
+
+            string res = "";
+            foreach (int item in array)
+                res += $"{item}, ";
+            res = res.Remove(res.Length-2);
+
+            Console.WriteLine("Дана последовательность:");
+            Console.WriteLine($"({res})");
+            Console.WriteLine($"Первый положительный: {firstPositive}");
+            Console.WriteLine($"Последний негативный: {lastNegative}");
+        }
+    }
+}
